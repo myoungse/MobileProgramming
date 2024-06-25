a60f533a78d7c3db5021019f4f58a34b68d326a3
myoungse <106461144+myoungse@users.noreply.github.com>
2024-06-25 22:28:01 +0900

    Create ResultActivity.java

---
+++ ResultActivity.java
@@ -0,0 +1,22 @@
+package com.example.project1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+        setTitle("투표 결과");
+
+        Intent intent = getIntent();
+        int[] voteResult = intent.getIntArrayExtra("VoteCount");
+        String[] imageName = intent.getStringArrayExtra("ImageName");
+
+    }
+}
